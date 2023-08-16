64ad470f06f2a4fbe42d99968dc8b885e6d389a3
SunhoLee <myshlee417@casys.kaist.ac.kr>
2023-08-16 07:56:19 +0000

    is_fin condition

--- software_layer/software_eff_conv.cpp
+++ software_layer/software_eff_conv.cpp
@@ -21,6 +21,26 @@ void software_request_generator::conv_fold_parallel_computation()
 		}
 	}
 
+	while(true)
+	{
+		//check_finish_or_not
+		bool is_fin=true;
+		for(int x=0;x<systolic_width;x++)
+		{
+			for(int y=0;y<systolic_height;y++)
+			{
+				int filter_index = local_filter_fold[x][y]*systolic_width+x;
+				int ifmap_index = local_ifmap_fold[x][y] * systolic_height + y;
+				//ifmap_idx, filter_idx condition.
+				if(ifmap_index<ifmap_iter_width*ifmap_iter_height && filter_index<filter_num)
+					is_fin=false;
+			}
+		}
+
+		if(is_fin)
+			break;
+	}
+
 
 	//free
 	for(int i=0;i<systolic_width;i++)
